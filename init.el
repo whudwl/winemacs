5580186214d72572cc0f1dd4db7fd9125679e72a
David <whudwl@gmail.com>
2022-07-15 13:57:05 +1000

    start server even when ~/.emacs.d/server is unsafe, without changing folder ownership

--- init.el
+++ init.el
@@ -68,3 +68,12 @@
 (defun use/magit()
   (interactive)
   (use-package magit))
+
+
+(require 'server)
+(when (and (>= emacs-major-version 23)
+           (equal window-system 'w32))
+  (defun server-ensure-safe-dir (dir) "Noop" t)) ; Suppress error "directory
+                                                 ; ~/.emacs.d/server is unsafe"
+                                                 ; on windows.
+(server-start)
